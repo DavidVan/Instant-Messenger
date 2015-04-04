fe9484d21d55e3bef7748dd0d2c1565880af95fd
David Van <David@DavidVan.net>
2015-04-04 02:32:05 -0700

    Made changes.

--- Instant Messenger/src/InstantMessenger.java
+++ Instant Messenger/src/InstantMessenger.java
@@ -1,6 +1,8 @@
 
 public class InstantMessenger {
 	public static void main(String[] args) {
-		imServer myServer = new imServer(1337, "127.0.0.1");
+		String host = args[0];
+		int port = Integer.parseInt(args[1]);
+		imServer myServer = new imServer(port, host);
 	}
 }
--- Instant Messenger/src/imServer.java
+++ Instant Messenger/src/imServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -23,7 +25,7 @@ public class imServer {
 			ServerSocket serverSocket = new ServerSocket(this.port, 0, InetAddress.getByName(this.host));;
 			Socket clientSocket = serverSocket.accept();
 			PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-			System.out.println(output);
+			BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
