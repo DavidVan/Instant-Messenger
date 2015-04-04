f2461c9f3aadbdfe682819a07fc624eb6b5acffd
David Van <David@DavidVan.net>
2015-04-04 06:49:00 -0700

    Added features.

--- IM Client/src/Client.java
+++ IM Client/src/Client.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Scanner;
 
 public class Client {
 	public String host;
 	public int port;
+	public Socket clientSocket;
+	public String userName;
 	
 	public Client() {
 		
@@ -10,7 +18,30 @@ public class Client {
 	public Client(String host, int port) {
 		this.host = host;
 		this.port = port;
+		Scanner sc = new Scanner(System.in);
+		userName = sc.nextLine();
+		sc.close();
+		try {
+			clientSocket = new Socket(host, port);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	public void initialConnection() {
+		try {
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			out.println(userName);
+			System.out.println(in.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
--- IM Server/src/Server.java
+++ IM Server/src/Server.java
@@ -1,14 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.ArrayList;
 
 public class Server {
 	public int port;
+	public ServerSocket serverSocket;
+	public ArrayList<Socket> clients;
 	
 	public Server() {
 		
 	}
 	
 	public Server(int port) {
+		clients = new ArrayList<Socket>();
 		this.port = port;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void connectClient() {
+		try {
+			clients.add(serverSocket.accept());
+			PrintWriter out = new PrintWriter(clients.get(clients.size()-1).getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String userName = in.readLine();
+			out.println("Welcome " + userName + "! Connection to server successful!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 }
