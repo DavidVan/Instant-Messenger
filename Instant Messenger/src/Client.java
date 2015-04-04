4cfc8fa10aa1f4cf5ca04b5ecd40c4b6e1b48189
Kenneth Ung <knnthung3@gmail.com>
2015-04-04 02:39:38 -0700

    Updated Client

--- Instant Messenger/src/Client.java
+++ Instant Messenger/src/Client.java
@@ -1,10 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Client {
-	
 	public Client(){
 		
 	}
 	public Client(int port, String host){
-		Socket scket = new Socket(host,port);
+		try {
+			Socket client = new Socket(host,port);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void recieve(Socket client){
+			BufferedReader in;
+			try {
+				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				System.out.print("String Recieved :");
+				while(!in.ready()){
+					System.out.println(in.readLine());
+					System.out.println();
+				}
+					in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 }
