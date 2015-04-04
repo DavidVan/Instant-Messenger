adea0d1c570a9e36d9e3af4b855801e420877b5b
David Van <David@DavidVan.net>
2015-04-04 02:05:11 -0700

    Made some changes...

--- Instant Messenger/src/InstantMessenger.java
+++ Instant Messenger/src/InstantMessenger.java
@@ -1,10 +1,6 @@
 
 public class InstantMessenger {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		imServer myServer = new imServer(1337, "127.0.0.1");
-=======
-		imServer myServer = new imServer(127.0.0.1, 1337); 
->>>>>>> 648d50e1d8d658adab155bf8f0b52a57a6f59aeb
 	}
 }
