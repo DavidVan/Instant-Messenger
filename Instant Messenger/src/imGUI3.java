42888f402b2957858024fb06247c5e2bb5191e2f
Ryan Ea <helloxhi1@gmail.com>
2015-04-04 22:20:22 -0700

    BOOO

--- Instant Messenger/src/imGUI3.java
+++ Instant Messenger/src/imGUI3.java
@@ -1,16 +1,32 @@
-import java.awt.Container;
-
-import javax.swing.JTabbedPane;
-
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class imGUI3 {
+	private JTabbedPane tabs= new JTabbedPane();
 	public imGUI3(){
-		JTabbedPane tabs= new JTabbedPane();
-		
+		JFrame window=new JFrame("IM WINDOW");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setSize(400, 500);
+		(window.getContentPane()).add(tabs);
+		add("temp");
+		add("five");
 	}
 	public Container Group(){
 		imtype imnew=new imtype();
 		return imnew.group();
+	
 	}
-
+	public void add(String name){
+		tabs.addTab(name, Group());
+	}
+	public void remove(String name){
+		if(tabs.indexOfTab(name)<0){
+			System.out.println("Failed");
+			return;
+		}
+		else
+			tabs.remove(tabs.indexOfTab(name));
+	} 
 }
